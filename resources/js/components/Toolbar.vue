6e65ecdc05354a0cf90b42f45d5296ff783de5e9
vagrant <vagrant@realtimeapp>
2019-10-14 23:04:34 +0000

    Login Component and login

--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -3,12 +3,14 @@
     <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
     <v-toolbar-title>Bitfumes</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
+    <div class="hidden-sm-and-down">
       <v-btn text>Forum</v-btn>
       <v-btn text>Ask Question</v-btn>
       <v-btn text>Category</v-btn>
-      <v-btn text>Login</v-btn>
-    </v-toolbar-items>
+      <router-link to="/login">
+          <v-btn text>Login</v-btn>
+      </router-link>
+    </div>
   </v-toolbar>
 </template>
 
